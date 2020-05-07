ea813e744424f827b5d6f561afdf286373747b72
ursu <ursualexandru96@gmail.com>
2020-05-07 23:54:46 +0300

    hello world v2

---
+++ src/main/java/ursu/disertatie/Controllers/HelloWorld.java
@@ -0,0 +1,16 @@
+package ursu.disertatie.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping(value = "/")
+    String getHelloWorld (){
+        return "Hello world v2!";
+    }
+
+}
+
+
